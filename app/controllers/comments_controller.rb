e0d61597f10f93eaa870e3fda2f98a671dfe04c7
lu <ques2309@hotmail.com>
2016-10-20 14:16:09 -0700

    My blog app

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "lou", password: "password",
+only: :destroy
+
+    def create
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.create(comment_params)
+	redirect_to article_path(@article)
+    end
+
+    def destory
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.find(params[:id])
+	@comment.destory
+	redirect_to article_path(@article)
+    end
+
+	private
+	  def comment_params
+		params.require(:comment).permit(:commenter, :body)
+	end
+end
